b9f5a50c9a6a5a2192f4f790fbf425ecdf1cddb9
Hyong <hyongguk1984@gmail.com>
2020-12-09 19:48:05 +0900

    third

--- server/index.js
+++
@@ -1,8 +0,0 @@
-const { setupServer } = require("./app");
-const app = setupServer();
-
-const port = process.env.PORT || 9000;
-
-app.listen(port, () => {
-  console.log(`We are lisning ${port}`);
-});
